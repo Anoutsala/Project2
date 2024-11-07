d516e7f8771d25e1ace795228110c52cd9cd35ab
Sara Hanmonty <anoutsala@berkeley.edu>
2024-11-06 16:57:45 -0800

    react added for homepage

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+
+function App() {
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
+
